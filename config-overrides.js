4d0564c5e46abfb9b2be8af3d4798137c207b02f
Jonáš Rosecký <rosecky.jonas@gmail.com>
2024-01-01 17:28:21 +0100

    Updated rewired to ignore source map warnings

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,18 @@
 module.exports = function override(config, env) {
-    config.optimization = {
-        splitChunks: {
-            chunks: "all",
-            minSize: 100000,
-            maxSize: 1000000,
+    // config.optimization = {
+    //     splitChunks: {
+    //         chunks: "all",
+    //         minSize: 100000,
+    //         maxSize: 1000000,
+    //     },
+    // };
+
+    config.ignoreWarnings = [
+        {
+            module: /node_modules/,
+            message: /Failed to parse source map/,
         },
-    };
+    ];
 
     return config;
 };
