435e32cca83f105b6924f85e9081dca87faedb00
Jonáš Rosecký <rosecky.jonas@gmail.com>
2024-04-08 15:49:30 +0200

    Changed default world generator to void

--- server/src/main/java/cz/xrosecky/terratinker/server/Session.java
+++ server/src/main/java/cz/xrosecky/terratinker/server/Session.java
@@ -2,6 +2,7 @@ package cz.xrosecky.terratinker.server;
 
 import cz.xrosecky.terratinker.Evaluator;
 import cz.xrosecky.terratinker.TerraTinker;
+import org.bukkit.Difficulty;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.WorldType;
@@ -28,9 +29,16 @@ public class Session {
         plugin.getServer().getScheduler().runTask(plugin, () -> {
             // Create a new world
             WorldCreator creator = new WorldCreator(id);
+            creator.type(WorldType.FLAT);
+            creator.generatorSettings("{\"layers\": [{\"block\": \"air\", \"height\": 1}], \"biome\":\"plains\"}");
             creator.environment(World.Environment.NORMAL);
+            creator.generateStructures(false);
             creator.generator(new VoidGen());
             world = plugin.getServer().createWorld(creator);
+            if (world == null) {
+                throw new RuntimeException("Could not create world");
+            }
+            world.setDifficulty(Difficulty.PEACEFUL);
             evaluator.evaluate(input, world);
         });
     }
