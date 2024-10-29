93c95c644677ac9f2dde73651c04f8466ca55895
Jonáš Rosecký <rosecky.jonas@gmail.com>
2024-10-29 13:00:26 +0100

    Added file selection to Local file node

--- web/src/nodes/loaders/LocalFileNode.tsx
+++ web/src/nodes/loaders/LocalFileNode.tsx
@@ -1,9 +1,21 @@
 // The method is actually a functional component. It is not a class component.
 /* eslint-disable react-hooks/rules-of-hooks */
 
-import { Box, Button, Spinner, Text } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    HStack,
+    IconButton,
+    Menu,
+    MenuButton,
+    MenuItemOption,
+    MenuList,
+    MenuOptionGroup,
+    Spinner,
+    Text,
+} from "@chakra-ui/react";
 import { useMemo, useState } from "react";
-import { BiFile, BiQuestionMark } from "react-icons/bi";
+import { BiFolderOpen, BiUpload } from "react-icons/bi";
 import { NodeProps } from "reactflow";
 import { useApi } from "../../api/ApiProvider";
 import {
@@ -17,6 +29,13 @@ import { Await } from "../../components/utils/Await";
 import { UploadButton } from "../../components/utils/uploadButton";
 import { GraphState } from "../../graphState/graphState";
 import { Node } from "../../types/layerTypes";
+
+const LOCALSTORAGE_KEY = "localFileNode_history";
+
+type LocalFileNodeData = {
+    id: string;
+    name: string;
+};
 
 export class LocalFileNode extends AbstractNode {
     static title = "Local File";
@@ -72,6 +91,14 @@ export class LocalFileNode extends AbstractNode {
             localFileNode.fileId === null ? 0 : 1
         );
 
+        const [history, setHistory] = useState<LocalFileNodeData[]>(() => {
+            const stored = localStorage.getItem(LOCALSTORAGE_KEY);
+            if (stored) {
+                return JSON.parse(stored);
+            }
+            return [];
+        });
+
         const onFile = async (file: File) => {
             if (!file) return;
 
@@ -108,70 +135,126 @@ export class LocalFileNode extends AbstractNode {
                 w={64}
                 helpPath="/nodes/loader/localFile"
             >
-                {!!localFileNode.fileId ? (
-                    <UploadButton
-                        size="lg"
-                        variant="outline"
-                        leftIcon={<BiFile />}
-                        mb={2}
-                        px={4}
-                        onFile={onFile}
-                    >
-                        <Box
-                            w="full"
-                            textAlign="left"
-                            overflow="hidden"
-                            whiteSpace="nowrap"
-                            textOverflow="ellipsis"
+                <Box
+                    mb={2}
+                    py={2}
+                    px={2}
+                    whiteSpace="nowrap"
+                    overflow="hidden"
+                    textOverflow="ellipsis"
+                >
+                    {fileInfo ? (
+                        <Await
+                            for={fileInfo!}
+                            loading={<Text>Fetching file info</Text>}
+                            error={(error) => (
+                                <Text color="red.500">
+                                    Error getting file info
+                                </Text>
+                            )}
                         >
-                            <Await
-                                for={fileInfo!}
-                                loading={<Spinner />}
-                                error={(error) => (
-                                    <Text color="red.500">
-                                        Error getting file info
-                                    </Text>
-                                )}
-                            >
-                                {(response) =>
-                                    response &&
-                                    response.data.status === "ok" ? (
-                                        response.data.data.filename
-                                    ) : (
+                            {(response) => {
+                                if (response && response.data.status === "ok") {
+                                    // Add to history
+                                    const fileData = response.data.data;
+                                    if (
+                                        !history.some(
+                                            (f) => f.id === fileData.uuid
+                                        )
+                                    ) {
+                                        const newHistory = [
+                                            ...history,
+                                            {
+                                                id: fileData.uuid,
+                                                name: fileData.filename,
+                                            },
+                                        ];
+
+                                        setHistory(newHistory);
+                                        localStorage.setItem(
+                                            LOCALSTORAGE_KEY,
+                                            JSON.stringify(newHistory)
+                                        );
+                                    }
+
+                                    return response.data.data.filename;
+                                } else {
+                                    return (
                                         <Text color="red.500">
                                             {response
                                                 ? response.data.message
                                                 : "Network error"}
                                         </Text>
-                                    )
+                                    );
                                 }
-                            </Await>
-                        </Box>
-                    </UploadButton>
-                ) : status > 0 ? (
-                    <Button
-                        size="lg"
-                        variant="outline"
-                        leftIcon={<Spinner />}
-                        mb={2}
-                        px={4}
-                    >
-                        {Math.floor(status * 100)}%
-                    </Button>
-                ) : (
-                    <UploadButton
-                        size="lg"
-                        variant="outline"
-                        leftIcon={<BiQuestionMark />}
-                        mb={2}
-                        px={4}
-                        onFile={onFile}
-                    >
-                        <Box w="full" textAlign="left">
-                            Select file
-                        </Box>
-                    </UploadButton>
-                )}
+                            }}
+                        </Await>
+                    ) : (
+                        <Text color="red.500">No file selected</Text>
+                    )}
+                </Box>
+                <HStack px={2} mb={4}>
+                    {status > 0 && status < 1 ? (
+                        <Button
+                            size="lg"
+                            leftIcon={<Spinner />}
+                            px={4}
+                            flexGrow={1}
+                        >
+                            {Math.floor(status * 100)}%
+                        </Button>
+                    ) : (
+                        <UploadButton
+                            size="lg"
+                            leftIcon={<BiUpload />}
+                            px={4}
+                            onFile={onFile}
+                            flexGrow={1}
+                        >
+                            <Box w="full" textAlign="left">
+                                Upload
+                            </Box>
+                        </UploadButton>
+                    )}
+                    {history.length > 0 && (
+                        <Menu>
+                            <MenuButton
+                                as={IconButton}
+                                icon={<BiFolderOpen />}
+                                aria-label="Browse"
+                                size="lg"
+                            >
+                                Actions
+                            </MenuButton>
+                            <MenuList maxW={96}>
+                                {history.map((file, i) => (
+                                    <MenuOptionGroup
+                                        value={
+                                            localFileNode.fileId || undefined
+                                        }
+                                    >
+                                        <MenuItemOption
+                                            key={i}
+                                            value={file.id}
+                                            onClick={() => {
+                                                localFileNode.fileId = file.id;
+                                                forceUpdate();
+                                            }}
+                                        >
+                                            <Text
+                                                whiteSpace="nowrap"
+                                                overflow="hidden"
+                                                textOverflow="ellipsis"
+                                            >
+                                                {file.name}
+                                            </Text>
+                                        </MenuItemOption>
+                                    </MenuOptionGroup>
+                                ))}
+                            </MenuList>
+                        </Menu>
+                    )}
+                </HStack>
                 {Object.entries(node.outputs).map(([id, output]) => (
                     <Variable
                         key={id}
