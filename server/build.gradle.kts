66471920c56a5cf73dfc86af6f72da374c3e63f3
Jonáš Rosecký <rosecky.jonas@gmail.com>
2026-02-23 10:34:21 +0100

    2.0.3

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "cz.xrosecky"
-version = "2.0.2"
+version = "2.0.3"
 description = "TerraTinker"
 
 var minecraftMajor = System.getenv().getOrDefault("MINECRAFT_MAJOR", "1").toInt()
